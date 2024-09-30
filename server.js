6e70ac9032af2f37786cade34be9146b561386bb
moshe212 <moshe212@gmail.com>
2024-09-30 22:55:20 +0300

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-const port = 3000;
+const port = 5000;
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -56,17 +56,29 @@ const pup = async (
   password,
   doRunAnAfterOneTime
 ) => {
-  // const an = "AN25MARTLVLON";
-  // const ss = "SS1G2";
-  // const mail = "148mor@gmail.com";
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--start-maximized"],
+  }); // Set headless: false to see the browser
 
-  const browser = await puppeteer.launch({ headless: false }); // Set headless: false to see the browser
   const page = await browser.newPage();
 
-  await page.goto(
-    "https://www.sellingplatformconnect.amadeus.com/login/?err=2026322",
-    { waitUntil: "networkidle0", timeout: 60000 }
-  ); // Replace with your URL
+  // const [page] = await browser.pages();
+  // const dimensions = await page.evaluate(() => {
+  //   return {
+  //     width: window.screen.availWidth,
+  //     height: window.screen.availHeight,
+  //   };
+  // });
+
+  // await page.setViewport(dimensions);
+
+  await page.setViewport({ width: 1920, height: 800 });
+
+  await page.goto("https://www.sellingplatformconnect.amadeus.com/login", {
+    waitUntil: "networkidle0",
+    timeout: 60000,
+  }); // Replace with your URL
 
   await page.waitForSelector("#usernameInput", { visible: true });
 
@@ -74,10 +86,6 @@ const pup = async (
   await page.type("#usernameInput", userName);
   await page.type("#officeIdInput", officeId);
   await page.type("#passwordInput", password);
-
-  // await page.type("#usernameInput", "3009922");
-  // await page.type("#officeIdInput", "TLVI32159");
-  // await page.type("#passwordInput", "moti2025");
 
   // Click the sign in button
   //   await Promise.all([
@@ -99,11 +107,17 @@ const pup = async (
   await page.keyboard.press("Enter");
   await page.evaluate(() => {
     return new Promise((resolve) => {
-      setTimeout(resolve, 5000); // waits for 1 second
+      setTimeout(resolve, 5000); // waits for 5 second
     });
   });
 
   let counter = 0;
+  let tabRunCounter = 0;
+  let tabNum = 1;
+  let tabOneStatus = true;
+  let tabTwoStatus = process.env.isTwoTabsWork;
+  let isNewTabOpen = false;
+
   const checkAvailability = async () => {
     console.log("Counter: ", counter);
 
@@ -112,7 +126,7 @@ const pup = async (
 
     await page.evaluate(() => {
       return new Promise((resolve) => {
-        setTimeout(resolve, 3000); // waits for 1 second
+        setTimeout(resolve, 3000); // waits for 3 second
       });
     });
 
@@ -128,7 +142,82 @@ const pup = async (
       sendMail(an, mail);
       // Do something after confirmation
       // await browser.close(); // Assuming 'browser' is accessible in this scope
-      return; // Stop further execution
+
+      switch (tabNum) {
+        case 1:
+          tabOneStatus = false;
+          break;
+        case 2:
+          tabTwoStatus = false;
+          break;
+      }
+
+      if (!tabOneStatus && !tabTwoStatus) {
+        return; // Stop further execution
+      }
+
+      if (!isNewTabOpen) {
+        // Click on the button that opens a new tab in the UI program
+        await page.click("#navbar_desktop_toolbar_new"); // Update the selector for your button
+        await page.evaluate(() => {
+          return new Promise((resolve) => {
+            setTimeout(resolve, 2000); // waits for 2 seconds
+          });
+        });
+
+        // await page.evaluate(() => {
+        //   const commandPageTab = Array.from(
+        //     document.querySelectorAll("li")
+        //   ).find((li) => li.getAttribute("title") === "Command page 2");
+        //   if (commandPageTab) {
+        //     commandPageTab.click();
+        //   }
+        // });
+
+        // Wait for a specific condition or selector
+        await page.waitForSelector(".cmdPrompt", {
+          visible: true,
+          timeout: 120000,
+        });
+
+        await page.type(".cmdPrompt", an);
+        await page.keyboard.press("Enter");
+        await page.evaluate(() => {
+          return new Promise((resolve) => {
+            setTimeout(resolve, 5000); // waits for 5 second
+          });
+        });
+
+        await checkAvailability();
+      } else {
+        const tabName = tabNum === 1 ? "Command page 2" : "Command page 1";
+        tabNum = tabNum === 1 ? 2 : 1;
+
+        // await page.evaluate((tabName) => {
+        //   const commandPageTab = Array.from(
+        //     document.querySelectorAll("span")
+        //   ).find((span) => span.innerText === tabName);
+        //   if (commandPageTab) {
+        //     commandPageTab.click();
+        //   }
+        // }, tabName);
+
+        // Wait for a specific condition or selector
+        await page.waitForSelector(".cmdPrompt", {
+          visible: true,
+          timeout: 120000,
+        });
+
+        await page.type(".cmdPrompt", an);
+        await page.keyboard.press("Enter");
+        await page.evaluate(() => {
+          return new Promise((resolve) => {
+            setTimeout(resolve, 5000); // waits for 5 second
+          });
+        });
+
+        await checkAvailability();
+      }
     } else {
       await page.evaluate(() => {
         return new Promise((resolve) => {
@@ -136,9 +225,82 @@ const pup = async (
         });
       });
 
+      // await page.click("#navbar_desktop_toolbar_new"); // Update the selector for your button
+
       counter++;
-      if (counter > 10) {
+      if (counter > 2) {
         counter = 0;
+        tabRunCounter++;
+
+        console.log("tabRunCounter", tabRunCounter);
+        if (tabRunCounter >= 2) {
+          tabRunCounter = 0;
+
+          if (
+            (tabNum === 1 && tabTwoStatus) ||
+            (tabNum === 2 && tabOneStatus)
+          ) {
+            // Switch to other tab
+            if (isNewTabOpen) {
+              console.log("new tab open");
+              const tabName =
+                tabNum === 1 ? "Command page 2" : "Command page 1";
+
+              console.log("tabNum", tabNum);
+              console.log("tabName", tabName);
+
+              await page.evaluate((tabName) => {
+                const commandPageTab = Array.from(
+                  document.querySelectorAll("div.c")
+                ).find((div) => {
+                  const span = div.querySelector("span");
+                  return span && span.innerText === tabName;
+                });
+                if (commandPageTab) {
+                  commandPageTab.click();
+                }
+              }, tabName);
+            } else {
+              console.log("new tab not open");
+              const tabName =
+                tabNum === 1 ? "Command page 2" : "Command page 1";
+
+              // Open new tab
+              await page.click("#navbar_desktop_toolbar_new"); // Update the selector for your button
+              isNewTabOpen = true;
+              // await page.evaluate((tabName) => {
+              //   const commandPageTab = Array.from(
+              //     document.querySelectorAll("span")
+              //   ).find((span) => span.innerText === tabName);
+              //   if (commandPageTab) {
+              //     commandPageTab.click();
+              //   }
+              // }, tabName);
+            }
+            tabNum = tabNum === 1 ? 2 : 1;
+            await page.evaluate(() => {
+              return new Promise((resolve) => {
+                setTimeout(resolve, 5000); // waits for 2 seconds
+              });
+            });
+            // Wait for a specific condition or selector
+            // await page.waitForSelector(".cmdPrompt", {
+            //   visible: true,
+            //   timeout: 120000,
+            // });
+
+            await page.type(".cmdPrompt", an);
+            await page.keyboard.press("Enter");
+            await page.evaluate(() => {
+              return new Promise((resolve) => {
+                setTimeout(resolve, 5000); // waits for 5 second
+              });
+            });
+
+            await checkAvailability();
+          }
+        }
+
         await page.type(".cmdPrompt", "ig");
         await page.keyboard.press("Enter");
         await page.evaluate(() => {
